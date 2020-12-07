745e817850c0557cd2ded930e6ad46618fe72b2e
alex198015 <kitten20811@gmail.com>
2020-12-07 18:58:53 +0200

    template ejs

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ const expressHbs = require('express-handlebars')
 
 const app = express()
 
-app.engine('hbs', expressHbs({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname:'hbs'
-}))
-app.set('view engine', 'hbs')
+// app.engine('hbs', expressHbs({
+//     layoutsDir: 'views/layouts/',
+//     defaultLayout: 'main-layout',
+//     extname:'hbs'
+// }))
+app.set('view engine', 'ejs')
+// app.set('view engine', 'hbs')
 // app.set('view engine', 'pug')
 app.set('views', 'views')
 app.use(bodyParser.urlencoded({ extended: false }))
